0b592449ebdf106622486abe3e345b7290470c0b
Lea Rosema <terabaud@gmail.com>
2021-07-09 17:13:02 +0200

    feat: add a skip link

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,10 @@
 const postcssImport = require('postcss-import');
-const postcssClamp = require('postcss-clamp');
 const autoprefixer = require('autoprefixer');
 const postcssCssVars = require('postcss-css-variables');
 
 module.exports = {
   plugins: [
     postcssImport({root: __dirname}),
-    postcssClamp(),
     postcssCssVars(),
     autoprefixer()
   ]
